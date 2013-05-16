4a561b8626f02f31a822ddaf6ee9216a7ba4f232
Wagner Pinheiro <wagner@lunatic.com.br>
2013-05-16 19:16:28 -0300

    * alteracoes no layout

--- web/menu.php
+++ web/menu.php
@@ -1,10 +1,12 @@
 <html>
 <head></head>
 <body>
+<div id="menu">
 Controles disponiveis:
 <ul>
 <li><a href="./document/">Documents</a></li>
 <li><a href="./fields/">Fields</a></li>
 </ul>
+</div>
 </body>
 </html>
