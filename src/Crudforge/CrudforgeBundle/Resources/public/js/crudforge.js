da3aae57a6aeee58977e7bb69e89fd26bd28a3b9
Wagner Pinheiro <wagner@lunatic.com.br>
2013-05-21 19:55:55 -0300

    * #43: added jquery, jquery ui, angular.js dependency * #16: better interface with jquery

---
+++ src/Crudforge/CrudforgeBundle/Resources/public/js/crudforge.js
@@ -0,0 +1,24 @@
+//default functions
+$(function(){
+    //theme buttons
+    $( ".cssButton" ).button();
+
+    $( "#accordion" ).accordion({ heightStyle: "fill" });
+
+    //theme tables
+    $("table th").each(function(){
+        $(this).addClass("ui-state-default");
+    });
+    $("table td").each(function(){
+        $(this).addClass("ui-widget-content");
+    });
+    $("table tr").hover(function(){
+        //$(this).children("td").addClass("ui-state-hover");
+     },function(){
+        //$(this).children("td").removeClass("ui-state-hover");
+     }
+    );
+    $("table tr").click(function(){
+        $(this).children("td").toggleClass("ui-state-highlight");
+    });
+});
